82accc62f778183d1607137ad3c6b47b94b6e131
TerryFunggg <terryyessfung@gmail.com>
2024-03-11 14:43:45 +0800

    add pretty indent message

--- lua/myextension/code-fold.lua
+++ lua/myextension/code-fold.lua
@@ -4,15 +4,37 @@
 
 local augroup = vim.api.nvim_create_augroup("CodeFold", { clear = true })
 
+local function custom_fold_text()
+    local v = vim.v
+    local fn = vim.fn
+
+    local foldLevel = string.rep("#", v.foldlevel)
+
+    local fold_line_count = v.foldend - v.foldstart + 1
+    local foldSize =  fold_line_count .. " lines "
+
+    local winwidth = vim.fn.winwidth(0)
+    local foldSizeStrWidth = vim.fn.strwidth(foldSize .. foldLevel)
+
+    local indent = string.rep(" ", winwidth - foldSizeStrWidth - 10) -- magic number 10 :)
+
+
+
+    return indent .. foldSize .. foldLevel
+end
 
 local function main()
     local vim = vim
     local opt = vim.opt
 
 
-    opt.foldmethod = "expr"
+    opt.foldmethod = "indent"
+    -- opt.foldmethod = "expr"
     opt.foldexpr = "nvim_treesitter#foldexpr()"
     opt.foldenable = false
+
+    opt.fillchars = { eob = "-", fold = " " }
+    opt.foldtext = "v:lua.require'myextension.code-fold'.custom_fold_text()"
 end
 
 local function setup()
@@ -26,4 +48,5 @@ end
 
 return {
     setup = setup,
+    custom_fold_text = custom_fold_text
 }
