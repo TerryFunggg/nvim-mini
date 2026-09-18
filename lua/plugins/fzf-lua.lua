0fd70e65ca3894aad86a8b04c09532ce94294b92
Terry Funggg <41190384+TerryFunggg@users.noreply.github.com>
2026-09-18 10:44:04 +0800

    fix: define fzf-lua keymaps via lazy keys using FzfLua commands

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,45 +1,51 @@
 return {
-	{
-		"ibhagwan/fzf-lua",
-		event = "VimEnter",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		opts = {
-			-- Match the old Telescope ivy look where it makes sense
-			winopts = {
-				height = 0.85,
-				width = 0.80,
-				row = 0.35,
-				col = 0.50,
-				border = "rounded",
-				preview = {
-					vertical = "down:45%",
-					horizontal = "right:50%",
-				},
-			},
-			files = {
-				file_ignore_patterns = {
-					"node_modules/",
-					"%.git/",
-					"venv/",
-					"vendor/",
-					"__pycache__",
-				},
-			},
-			grep = {
-				file_ignore_patterns = {
-					"node_modules/",
-					"%.git/",
-					"venv/",
-					"vendor/",
-					"__pycache__",
-				},
-			},
-		},
-		config = function(_, opts)
-			local fzf = require("fzf-lua")
-			fzf.setup(opts)
-		end,
-	},
+  {
+    "ibhagwan/fzf-lua",
+    event = "VimEnter",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find Files" },
+      { "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "Find Buffer" },
+      { "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "Grep" },
+      { "<leader>fp", "<cmd>FzfLua git_files<cr>", desc = "Find Git" },
+      { "<leader>fh", "<cmd>FzfLua help_tags<cr>", desc = "Help" },
+      { "<leader>fk", "<cmd>FzfLua keymaps<cr>", desc = "Keys" },
+      { "<leader>fc", "<cmd>FzfLua files cwd=" .. vim.fn.stdpath("config") .. "<cr>", desc = "Search Nvim Config" },
+      { "<leader>/", "<cmd>FzfLua blines<cr>", desc = "Fuzzily search" },
+    },
+    opts = {
+      -- Match the old Telescope ivy look where it makes sense
+      winopts = {
+        height = 0.85,
+        width = 0.80,
+        row = 0.35,
+        col = 0.50,
+        border = "rounded",
+        preview = {
+          vertical = "down:45%",
+          horizontal = "right:50%",
+        },
+      },
+      files = {
+        file_ignore_patterns = {
+          "node_modules/",
+          "%.git/",
+          "venv/",
+          "vendor/",
+          "__pycache__",
+        },
+      },
+      grep = {
+        file_ignore_patterns = {
+          "node_modules/",
+          "%.git/",
+          "venv/",
+          "vendor/",
+          "__pycache__",
+        },
+      },
+    },
+  },
 }
