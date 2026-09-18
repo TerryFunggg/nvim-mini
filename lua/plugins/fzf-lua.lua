b65d6808a9cf770f2ba36c754b306d532abc615e
Terry Funggg <41190384+TerryFunggg@users.noreply.github.com>
2026-09-18 10:43:10 +0800

    Remove keymaps from fzf-lua plugin spec; they now live in keymaps.lua

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -40,22 +40,6 @@ return {
 		config = function(_, opts)
 			local fzf = require("fzf-lua")
 			fzf.setup(opts)
-
-			-- Keep the same leader keys you had with Telescope
-			vim.keymap.set("n", "<leader>hh", fzf.help_tags, { desc = "Help" })
-			vim.keymap.set("n", "<leader>hk", fzf.keymaps, { desc = "Keys" })
-			vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find Files" })
-			vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Find Buffer" })
-			vim.keymap.set("n", "<leader>fp", fzf.git_files, { desc = "Find Git" })
-			vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Grep" })
-
-			vim.keymap.set("n", "<leader>/", function()
-				fzf.blines({ winopts = { height = 0.40, preview = { hidden = "hidden" } } })
-			end, { desc = "Fuzzily search" })
-
-			vim.keymap.set("n", "<leader>fc", function()
-				fzf.files({ cwd = vim.fn.stdpath("config") })
-			end, { desc = "Search Nvim Config" })
 		end,
 	},
 }
