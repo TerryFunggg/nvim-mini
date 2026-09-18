b4c1cf52c31a1dd6888d093309e78a565fb5a526
Terry Funggg <41190384+TerryFunggg@users.noreply.github.com>
2026-09-18 17:06:41 +0800

    Merge mini.pick setup into mini.nvim plugin and add keymaps
    Co-authored-by: Grok 4.6
    Reviewed-by: Terry Fung

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,6 +10,14 @@ return {
 			statusline.section_location = function()
 				return "%2l:%-2v"
 			end
+
+			require("mini.pick").setup()
 		end,
+		keys = {
+			{ "<leader>ff", function() require("mini.pick").builtin.files() end, desc = "Find files" },
+			{ "<leader>fg", function() require("mini.pick").builtin.grep_live() end, desc = "Live grep" },
+			{ "<leader>fb", function() require("mini.pick").builtin.buffers() end, desc = "Find buffers" },
+			{ "<leader>fh", function() require("mini.pick").builtin.help() end, desc = "Help tags" },
+		},
 	},
 }
